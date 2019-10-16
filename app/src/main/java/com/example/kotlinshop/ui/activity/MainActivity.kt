c6e67b3d8a5a0c5e0a46d57d4fd612b0f6030933
setsu <1208280008@qq.com>
2019-10-16 19:50:11 +0900

    Bannerの作成

--- app/src/main/java/com/example/kotlinshop/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlinshop/ui/activity/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.kotlinshop
+package com.example.kotlinshop.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinshop.R
+import com.example.kotlinshop.ui.fragment.HomeFragment
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mBottomNavBar.checkCartBadge(20)
         mBottomNavBar.checkMsgBadge(false)
+        initview()
 
         Observable.timer(2,TimeUnit.SECONDS)
             . observeOn(AndroidSchedulers.mainThread())
@@ -24,6 +26,10 @@ class MainActivity : AppCompatActivity() {
             . observeOn(AndroidSchedulers.mainThread())
             .subscribe({mBottomNavBar.checkMsgBadge(true)})
 
+    }
+    private fun initview(){
+        val  manager=supportFragmentManager.beginTransaction()
+        manager.replace(R.id.mContaier,HomeFragment()).commit()
     }
 
 }
